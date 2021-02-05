34656a8a8f84e3a72dac9fa6149190c97d54dcfd
Rafal Witczak <rwitczak89@gmail.com>
2021-02-06 00:21:01 +0100

    refactor(app): remove unnecessary imports

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,6 @@ import Home from "./home";
 import LaunchPads from "./launch-pads";
 import LaunchPad from "./launch-pad";
 import FavouritesDrawer from "./favourites";
-import ModifyFavouritesButton from "./modify-favourites-button";
 
 export default function App() {
   return (
@@ -45,7 +44,6 @@ function NavBar() {
         >
           ¡SPACE·R0CKETS!
         </Text>
-
         <FavouritesDrawer />
       </Flex>
     </>
