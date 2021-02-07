326570ea6254ceedcbf4933df28e9e35be184bf0
Rafal Witczak <rwitczak89@gmail.com>
2021-02-08 00:56:29 +0100

    feat(timeline): add details to timeline
    toggle details on topic click

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,10 @@
-import { Box, Flex, Text } from "@chakra-ui/core";
-import React from "react";
+import { Box, Button, Flex, Text } from "@chakra-ui/core";
+import { Link } from "@chakra-ui/react";
+import { CloseIcon, ExternalLinkIcon } from "@chakra-ui/icons";
+import React, { useState } from "react";
 import { getDayMonthYearString } from "../utils/format-date";
 import { useSpaceX } from "../utils/use-space-x";
+import Breadcrumbs from "./breadcrumbs";
 import Error from "./error";
 
 export default function Timeline() {
@@ -12,7 +15,12 @@ export default function Timeline() {
 
   return (
     <>
-      <button onClick={() => console.log(items)}>CONSOLE LOG</button>
+      <Breadcrumbs
+        items={[
+          { label: "Home", to: "/" },
+          { label: "History", to: ".." },
+        ]}
+      />
 
       <Flex direction="column" alignItems="center" m={"2rem"}>
         {items
@@ -24,40 +32,116 @@ export default function Timeline() {
 }
 
 function TimelineElement({ item }) {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <Flex direction="column" alignItems="center" fontWeight="700">
-      <Text
-        color="gray.700"
-        fontSize="md"
-        alignSelf="center"
-        transform="translateY(20vh)"
-        zIndex={100}
-        backgroundColor="#fff"
-        p={"0.5rem"}
-        m={0}
-      >
-        {getDayMonthYearString(item.event_date_utc)}
-      </Text>
+    <Flex direction="column" alignItems="center">
       <Box
         backgroundColor="#1A202C"
-        borderRadius="0%"
         height="40vh"
         width="0.2rem"
         mb={"-38vh"}
         alignSelf="center"
       ></Box>
-      <Flex>
-        <Box
-          backgroundColor="gray.300"
-          boxShadow="md"
-          borderWidth="1px"
-          rounded="lg"
-          height="40vh"
-          width="40vw"
-          transform={item.id % 2 === 0 ? "translateX(-70%)" : "translateX(70%)"}
-          mt={"-1rem"}
-        ></Box>
+      <Flex height="30vh" direction="column">
+        <Text
+          p={"1rem"}
+          fontFamily="mono"
+          letterSpacing="0.2px"
+          fontWeight="md"
+          fontSize="md"
+          alignSelf="center"
+          backgroundColor="#fff"
+        >
+          {getDayMonthYearString(item.event_date_utc)}
+        </Text>
+        <Button
+          backgroundColor="#1A202C"
+          color="#fff"
+          p={"0.5rem"}
+          fontFamily="mono"
+          letterSpacing="0.5px"
+          fontWeight="bold"
+          fontSize="md"
+          _hover={{
+            backgroundColor: "#fff",
+            color: "#1A202C",
+          }}
+          _focus={{ outline: "none" }}
+          _active={{ outline: "none", transform: "scale(0.98)" }}
+          onClick={() => setShowDetails(true)}
+        >
+          {item.title}
+        </Button>
       </Flex>
+
+      {showDetails ? (
+        <Flex
+          p={"1rem"}
+          overflow="scroll"
+          direction="column"
+          position="absolute"
+          maxHeight={"40vh"}
+          maxWidth={"27vw"}
+          boxShadow="md"
+          rounded="lg"
+          backgroundColor="#fff"
+          color="#1A202C"
+          transform={
+            item.id % 2 === 0 ? "translate(-30vw, 0)" : "translate(30vw, 0)"
+          }
+          zIndex={100}
+        >
+          <Button
+            position="absolute"
+            right={0}
+            top={0}
+            p={"0.2rem"}
+            backgroundColor="transparent"
+            _hover={{
+              backgroundColor: "transparent",
+              transform: "scale(1.1)",
+            }}
+            _active={{ transform: "scale(0.90)" }}
+            _focus={{ outline: "none" }}
+            onClick={() => setShowDetails(false)}
+            alignSelf="flex-end"
+            color="#1A202C"
+          >
+            <CloseIcon />
+          </Button>
+          <Text
+            fontFamily="mono"
+            letterSpacing="0.2px"
+            fontWeight="bold"
+            fontSize="xs"
+            backgroundColor="#fff"
+            color="#1A202C"
+            p={"0.5rem"}
+          >
+            {item.title}
+          </Text>
+          <Text
+            fontFamily="mono"
+            letterSpacing="0.2px"
+            fontWeight="md"
+            fontSize="xs"
+          >
+            {item.details}
+          </Text>
+          <Link href={item.links.article}>
+            <Text
+              p={"1rem"}
+              fontFamily="mono"
+              letterSpacing="0.2px"
+              fontWeight="md"
+              fontSize="xs"
+            >
+              Go to source <ExternalLinkIcon mx="2px" />
+            </Text>
+          </Link>
+        </Flex>
+      ) : null}
     </Flex>
   );
 }
